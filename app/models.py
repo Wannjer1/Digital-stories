251b1373d14bafc0f209ff7c585a13257bb0ee58
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-18 21:10:42 +0300

    created relationship between user and role class

--- app/models.py
+++ app/models.py
@@ -20,6 +20,7 @@ class User(UserMixin, db.Model):
   password_hash = db.Column(db.String(255))
   bio = db.Column(db.String(255))
   profile_pic_path = db.Column(db.String(255))
+  role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
  
   
   @property
@@ -40,6 +41,7 @@ class Role(db.Model):
   __tablename__ = 'roles'
   id = db.Column(db.Integer, primary_key=True)
   name= db.Column(db.String(255))
+  users = db.relationship('User',backref = 'role', lazy = "dynamic")
 
   def __repr__(self):
     return f'User {self.name}'
