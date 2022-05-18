d373fe1654ffdf96a08e7d3289741704abc3b78a
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-19 02:02:31 +0300

    added route to profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template,request,redirect,url_for,abort
 from flask_login import login_required, current_user
+from ..models import User
 from datetime import datetime as dt
 
 from .. import db,photos
@@ -12,3 +13,12 @@ def index():
   title = 'One place, Many Stories'
  
   return render_template('index.html', title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+  user = User.query.filter_by(username=uname).first()
+
+  if user is None:
+    abort(404)
+
+  return render_template('profile/profile.html', user=user)
