6342acd997d5f4dc7b82262192486207027397da
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-19 00:17:05 +0300

    added route to login forms

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
+from flask_login import login_user
 
 
 # route to registration form
@@ -22,3 +23,20 @@ def register():
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+# route to login forms
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "ModernStories login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
+
+# 
