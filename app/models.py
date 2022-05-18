54dfd2c127c565ae756ec123da75d57b13430729
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-19 02:01:29 +0300

    updates user class to store user profile

--- app/models.py
+++ app/models.py
@@ -17,9 +17,9 @@ class User(UserMixin, db.Model):
   last_name = db.Column(db.String(255))
   username = db.Column(db.String(255), unique=True, index=True)
   email = db.Column(db.String(255), unique=True, index=True)
-  password_hash = db.Column(db.String(255))
   bio = db.Column(db.String(255))
   profile_pic_path = db.Column(db.String(255))
+  password_hash = db.Column(db.String(255))
   role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
  
   
