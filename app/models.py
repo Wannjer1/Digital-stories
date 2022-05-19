ccbab59fe29221b4e70b9aca12f7b09a8a86cb14
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-19 23:31:49 +0300

    created blog class model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
+
+from unicodedata import category
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin, current_user
 from datetime import datetime
-from . import db
-from . import login_manager
+from . import db,login_manager
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -20,8 +21,8 @@ class User(UserMixin, db.Model):
   bio = db.Column(db.String(255))
   profile_pic_path = db.Column(db.String(255))
   password_hash = db.Column(db.String(255))
-  role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-  blog = db.relationship('Blog',backref='user', lazy='dynamic')
+  role_id = db.Column(db.Integer,db.ForeignKey('roles.id'),nullable=False)
+  blog_user = db.relationship('Blog',backref='user', lazy='dynamic')
  
   
   @property
@@ -34,12 +35,12 @@ class User(UserMixin, db.Model):
     
   def verify_password(self, password):
     return check_password_hash(self.password_hash, password)
-
-  
   
   def __repr__(self):
       return f'User {self.username}'
 
+    
+# model for roles
 class Role(db.Model):
   __tablename__ = 'roles'
   id = db.Column(db.Integer, primary_key=True)
@@ -50,52 +51,63 @@ class Role(db.Model):
     return f'User {self.name}'
 
   # blog class
-class Blog(db.Model):
-  __tablename__ = 'blog'
-  id = db.Column(db.Integer, primary_key = True)
-  title = db.Column(db.String(255),nullable=False)
-  content = db.Column(db.Text(),nullable=False)
-  posted = db.Column(db.DateTime,default=datetime.utcnow)
-  user_id = db.Column(db.Integer,db.ForeignKey("users.id")) 
 
-  def save(self):
+class Blog(db.Model):
+  ''''''
+  __tablename__ = 'blogs'
+
+  id = db.Column(db.Integer, primary_key = True)
+  blog_image= db.Column(db.String,nullable=False)
+  title = db.Column(db.String(255),nullable=False)
+  category = db.Column(db.String(255),nullable=False)
+  owner_id = db.Column(db.Integer,db.ForeignKey("users.id"),nullable=False)
+  posted = db.Column(db.DateTime,default=datetime.utcnow,nullable=False)
+  content = db.Column(db.String(),nullable=False)
+  
+
+  def save_blog(self):
     db.session.add(self)
     db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+#   # def delete(self):
+#   #   db.session.delete(self)
+#   #   db.session.commit()
 
-  def get_blog(id):
-    blog = Blog.query.filter_by(id=id).first()
-    return blog
+#   # def get_blog(id):
+#   #   blog = Blog.query.filter_by(id=id).first()
+#   #   return blog
+
+  # @classmethod
+  # def get_blog(cls, id):
+  #   blogs = Blog.query.filter_by(id=id).first()
+  #   return blogs
 
   def __repr__(self):
-    return f'Blog {self.title}'
+    return f'Blog {self.category}'
 
 # comments model
-class Comment(db.Model):
-  __tablename__ = 'comments'
-  id = db.Column(db.Integer,primary_key=True)
-  comment = db.Column(db.String)
-  posted = db.Column(db.DateTime,default=datetime.datetime.utcnow)
-  blog_id = db.Colun(db.Integer,db.ForeignKey('blogs.id'))
-  user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+# class Comment(db.Model):
+#   __tablename__ = 'comments'
+#   id = db.Column(db.Integer,primary_key=True)
+#   comment = db.Column(db.String)
+#   posted = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+#   blog_id = db.Colun(db.Integer,db.ForeignKey('blogs.id'))
+#   user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
-  def save(self):
-    db.session.add(self)
-    db.session.commit()
+#   def save(self):
+#     db.session.add(self)
+#     db.session.commit()
 
-  def delete(self):
-    db.session.remove(self)
-    db.session.commit()
+#   def delete(self):
+#     db.session.remove(self)
+#     db.session.commit()
 
-  def get_comment(id):
-    comment = Comment.query.all(id=id)
-    return comment
+#   def get_comment(id):
+#     comment = Comment.query.all(id=id)
+#     return comment
 
-  def __repr__(self):
-    return f'Comment {self.comment}'
+#   def __repr__(self):
+#     return f'Comment {self.comment}'
 
 
 
