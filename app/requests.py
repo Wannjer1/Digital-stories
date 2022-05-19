731cbe4bf28de82b4740e4b6b59cd55016ee1903
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-20 01:13:55 +0300

    fetched random quote api

--- app/requests.py
+++ app/requests.py
@@ -1,7 +1,23 @@
-secret_key = None
+from .models import Quote
+import urllib.request, json 
+
+Random_Quote_Url='http://quotes.stormconsultancy.co.uk/random.json'
 
 
-def configure_request(app):
-  '''function that configures the request object needed for running the application'''
-  global secret_key
-  secret_key = app.config['SECRET_KEY']
+def get_quote():
+    '''Function that fetches the random json quote'''
+
+    url = Random_Quote_Url
+
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+
+    quote_details = []
+
+    author = data.get('author')
+    quote = data.get('quote')
+
+    new_quote = Quote(author,quote)
+    quote_details.append(new_quote)
+
+    return quote_details
