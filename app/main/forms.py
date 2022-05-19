00b8247e410cc622c1267982fc8146e9b931045b
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-19 03:28:02 +0300

    added email validator to profile update class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,TextAreaField
-from wtforms.validators import InputRequired,Length
+from wtforms import StringField,SubmitField,TextAreaField,ValidationError
+from wtforms.validators import InputRequired,Email
 from ..models import User
+from flask_login import current_user
 
 class UpdateProfile(FlaskForm):
-    username = StringField('New username',validators = [InputRequired])
+    username = StringField('New username',validators = [InputRequired()])
+    email = StringField('New email',validators = [InputRequired(),Email()])
     bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
     submit = SubmitField('Submit')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            if User.query.filter_by(email = email.data).first():
+                raise ValidationError("The Email has already been taken!")
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            if User.query.filter_by(username = username.data).first():
+                raise ValidationError("The username has already been taken")
